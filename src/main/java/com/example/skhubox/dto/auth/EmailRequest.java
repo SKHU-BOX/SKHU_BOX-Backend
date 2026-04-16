b00cc731ff97b24968acda6b715284ebb01c6fce
Kang, DaeHyuk <kangdh0430@naver.com>
2026-04-17 02:57:49 +0900

    feat: implement email verification using Redis and JavaMailSender

---
+++ src/main/java/com/example/skhubox/dto/auth/EmailRequest.java
@@ -0,0 +1,14 @@
+package com.example.skhubox.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EmailRequest {
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
+}
